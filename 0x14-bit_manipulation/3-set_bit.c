55a74fd4ccafc87316857a84f2bca6991dd2ed2f
Victor Archie <112628604+victor-archie@users.noreply.github.com>
2023-06-27 04:34:17 +0100

    Create 3-set_bit.c

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * set_bit - sets the value of a bit to 1 at a given index.
+ * @n: number to set
+ * @index: index at which to set bit
+ *
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8 - 1)
+	{
+		return (-1);
+	}
+
+	*n |= 1UL << index;
+
+	return (0);
+}
